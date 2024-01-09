6b2c0af80128cd4f59f943d75f77c4416d70862f
Tamil Bharathi K <111305647+TamilBharathik@users.noreply.github.com>
2024-01-09 18:54:12 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Product()
+        {
+           
+            Name = "DefaultProductName";
+        }
+    }
+}
